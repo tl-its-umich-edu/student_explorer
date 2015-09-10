a1a734f3ecf365fcd0135bf20bb6a93cb8a7b913
weikaizh <weikaizh@umich.edu>
2015-09-10 15:17:00 -0400

    removed model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-	name = models.CharField(max_length=50)
-	student_ID = models.CharField(max_length=10)
-	status = models.TextField(blank=True, null=True)
-	cohorts = models.TextField(blank=True, null=True)
-	GPA = models.DecimalField(max_digits=4, decimal_places=3)
-	year = models.CharField(max_length=10)
