a04d599f7e029b509abc09264160f9f1a0efaf30
Kris Steinhoff <steinhof@umich.edu>
2015-09-14 15:27:53 -0400

    Add relationships to serializers

--- advising/views.py
+++ advising/views.py
@@ -2,13 +2,6 @@ from advising.models import Advisor, Student
 from advising.serializers import AdvisorSerializer, StudentSerializer
 from rest_framework import viewsets
 
-# from django.http import HttpResponse
-# import json
-#
-# def advisor_list(request):
-#     data = models.Advisor.objects.all()
-#     return HttpResponse(json.dumps(data), content_type="application/json")
-
 
 class AdvisorViewSet(viewsets.ReadOnlyModelViewSet):
     '''
@@ -23,4 +16,4 @@ class StudentViewSet(viewsets.ReadOnlyModelViewSet):
     API endpoint that allows students to be viewed.
     '''
     queryset = Student.objects.all()
-    serializer_class = AdvisorSerializer
+    serializer_class = StudentSerializer
