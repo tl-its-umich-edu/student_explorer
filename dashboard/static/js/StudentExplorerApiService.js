c02da892e41d6efadd33cb26bf3510e32326ca8a
weikaizh <weikaizh@umich.edu>
2015-09-24 11:21:35 -0400

    fixed ng-cloak problem

--- dashboard/static/js/StudentExplorerApiService.js
+++ dashboard/static/js/StudentExplorerApiService.js
@@ -14,7 +14,7 @@
                             console.log(entry)
                             // entry.status = [{engage: Math.floor(Math.random()*5), explore: Math.floor(Math.random()*5), encourage: Math.floor(Math.random()*5)}]
                             entry.GPA = (Math.random()*4).toFixed(1)
-                            entry.year = selectYear()
+                            // entry.year = selectYear()
                             data.push(entry)
                         });
                         console.log(data);
