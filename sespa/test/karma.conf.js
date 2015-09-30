6cb7d5c684a983792de6d287080d2e997958061e
Kris Steinhoff <steinhof@umich.edu>
2015-09-30 15:10:43 -0400

    Remove angular-rangeslider (for now)

--- sespa/test/karma.conf.js
+++ sespa/test/karma.conf.js
@@ -31,7 +31,6 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-rangeslider/angular.rangeSlider.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
