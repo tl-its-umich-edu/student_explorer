388fe0f3d9400d3f1d24519dda1502b5483a30b3
weikaizh <weikaizh@umich.edu>
2015-10-02 10:25:34 -0400

    added dependencies in bower.json

---
+++ sespa/app/scripts/services/studentservice.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name sespaApp.StudentExplorerApiService
+ * @description
+ * # StudentExplorerApiService
+ * Factory in the sespaApp.
+ */
+'use strict';
+
+angular.module('sespaApp')
+       .factory('StudentExplorerApiService', function($http,$q) {
+          var seapiService = {
+            students: function(student) {
+              var promise = $http.get('http://localhost:2080/api/students/')
+                              .then(function(response) {
+                                  var data = []
+                                  response.data.results.forEach(function(entry) {
+                                      console.log(entry)
+                                      // entry.status = [{engage: Math.floor(Math.random()*5), explore: Math.floor(Math.random()*5), encourage: Math.floor(Math.random()*5)}]
+                                      // entry.GPA = (Math.random()*4).toFixed(1)
+                                      // entry.year = selectYear()
+                                      data.push(entry)
+                                  });
+                                  console.log(data);
+                                  return data;
+                              });
+              return promise;
+            },
+            student: function(student) {
+              var promise = $http.get('http://localhost:2080/api/students/'+student+'/full/')
+                              .then(function(response) {
+                                return response.data;
+                              });
+              return promise;
+            },
+            advisors: function(advisor) { 
+              var promise = $http.get('http://localhost:2080/api/advisors/')
+                              .then(function(response) {
+                                  var data = []
+                                  response.data.results.forEach(function(entry) {
+                                      console.log(entry)
+                                      data.push(entry)
+                                  });
+                                  console.log(data);
+                                  return data;
+                              });
+              return promise;
+            },
+            advisor: function(advisor) { 
+              var promise = $http.get('http://localhost:2080/api/advisors/'+advisor+'/')
+                              .then(function(response) {
+                                  return response.data;
+                              });
+              return promise;
+            }
+          };
+          return seapiService;
+       });
+
+function selectYear(){
+  var num = Math.floor(Math.random()*4);
+  if (num == 0) {
+    return "Freshman";
+  } else if (num == 1) {
+    return "Sophomore";
+  } else if (num == 2) {
+    return "Junior";
+  } else if (num == 3) {
+    return "Senior";
+  } else {
+    return selectYear();
+  }
+}
