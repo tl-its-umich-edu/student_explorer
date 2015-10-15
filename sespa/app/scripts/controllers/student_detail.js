7ff3c3a446384ce8599b8aabd26e3c210d936e62
weikaizh <weikaizh@umich.edu>
2015-10-15 13:36:48 -0400

    Implement student detail page

--- sespa/app/scripts/controllers/student_detail.js
+++ sespa/app/scripts/controllers/student_detail.js
@@ -8,10 +8,14 @@
  * Controller of the sespaApp
  */
 angular.module('sespaApp')
-  .controller('StudentDetailCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('StudentDetailCtrl', function(advisingData, $scope, $routeParams) {
+  	$scope.student = null;
+  	$scope.sortType = 'class_site';
+  	$scope.advisors = null;
+
+  	advisingData.studentDetails($routeParams.student).then(function(student) {
+      $scope.student = student;
+      $scope.advisors = student.advisors;
+    });
+
   });
