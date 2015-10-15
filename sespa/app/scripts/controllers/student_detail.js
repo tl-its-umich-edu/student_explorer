c5f5ef6d60b373961432a8b478d0ae5f1f9c219c
weikaizh <weikaizh@umich.edu>
2015-10-15 10:17:44 -0400

    Add student_detail controller and view

---
+++ sespa/app/scripts/controllers/student_detail.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sespaApp.controller:StudentDetailCtrl
+ * @description
+ * # StudentDetailCtrl
+ * Controller of the sespaApp
+ */
+angular.module('sespaApp')
+  .controller('StudentDetailCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
