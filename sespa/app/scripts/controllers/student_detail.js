40b8ce4bffdf52efe55f382643169a6ed5df89d2
weikaizh <weikaizh@umich.edu>
2015-10-16 08:35:17 -0400

    Add a message when no course is found for a student

--- sespa/app/scripts/controllers/student_detail.js
+++ sespa/app/scripts/controllers/student_detail.js
@@ -13,9 +13,14 @@ angular.module('sespaApp')
   	$scope.sortType = 'class_site';
   	$scope.advisors = null;
 
+  	$scope.hasNoCourse = false;
+
   	advisingData.studentDetails($routeParams.student).then(function(student) {
       $scope.student = student;
       $scope.advisors = student.advisors;
+      if ($scope.student.class_sites.length == 0) {
+      	$scope.hasNoCourse = true;
+      }
     });
 
   });
