9b7651d45f3c52dbaa78f1b6aeaf77505e061ef2
Kris Steinhoff <steinhof@umich.edu>
2015-10-17 22:52:45 -0400

    Remove old (server-side) interface

--- dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('dashboard.views',
-                       url(r'^$', 'dashboard', name='dashboard'),
-                       url(r'^student/(?P<student>[a-zA-Z]+)/$', 'studentPage', name='studentPage'),
-                       url(r'^advisors/$', 'advisorDashboard', name='advisorDashboard'),
-                       url(r'^advisors/(?P<advisor>[a-zA-Z]+)/students/$', 'dashboard', name='dashboard'),
-                       )
