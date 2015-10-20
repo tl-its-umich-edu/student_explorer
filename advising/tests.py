b2328ad776d0da6f2e5bbe961299d00cfdff24a2
Kris Steinhoff <steinhof@umich.edu>
2015-10-20 11:29:54 -0400

    Add some unit tests for advising

--- advising/tests.py
+++ advising/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
+import json
 
-# Create your tests here.
+
+class AdvisingTestCase(TestCase):
+    fixtures = ['dev_data.json']
+    client = None
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_bad_url(self):
+        response = self.client.get(
+            '%s/rubarb-rubarb-rubarb/' % reverse('advising-api-root'))
+
+        self.assertEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_api_root_unauthenticated(self):
+        response = self.client.get(reverse('advising-api-root'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 500)
+
+    def test_api_config_username_unauthenticated(self):
+        response = self.client.get(reverse('advising-api-root'))
+        data = json.loads(response.content)
+
+        self.assertIsNone(data['username'], 'username should be None')
+
+    def test_advisors(self):
+        response = self.client.get(reverse('advisor-list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 500)
