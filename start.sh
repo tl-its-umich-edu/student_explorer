2b823114a0c4aa99c324ce16427b81dd0068c148
Kris Steinhoff <steinhof@umich.edu>
2015-11-06 09:29:04 -0500

    Add start.sh script to use in production

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ -z "${GUNICORN_WORKERS}" ]; then
+    GUNICORN_WORKERS=2
+fi
+
+python manage.py migrate
+# python manage.py collectstatic
+
+gunicorn --workers=${GUNICORN_WORKERS} \
+    --bind=0.0.0.0:80 student_explorer.wsgi:application
