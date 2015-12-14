6c188657078997fe64489aaf6193cd0808a56a5e
Kris Steinhoff <steinhof@umich.edu>
2015-12-14 13:03:21 -0500

    Add default login url

--- student_explorer/settings/base.py
+++ student_explorer/settings/base.py
@@ -108,6 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/api/api-auth/login/'
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': None,
