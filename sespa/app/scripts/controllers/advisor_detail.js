eddcf411d675c1dd00b3ce6deba813ac351ff010
Kris Steinhoff <steinhof@umich.edu>
2016-01-21 15:19:53 -0500

    Better handing for non-advisor users on main page

--- sespa/app/scripts/controllers/advisor_detail.js
+++ sespa/app/scripts/controllers/advisor_detail.js
@@ -30,14 +30,18 @@ angular.module('sespaApp')
       advisingData.advisorDetails(advisorUsername).then(function(advisor) {
         $scope.advisor = advisor;
       }, function(reason) {
-        advisingUtilities.httpErrorHandler(reason, $scope);
+        if (reason.status !== 404) {
+          advisingUtilities.httpErrorHandler(reason, $scope);
+        }
       });
 
       advisingData.advisorsStudents(advisorUsername).then(function(students) {
         $scope.progress = 100;
         $scope.students = students;
       }, function(reason) {
-        advisingUtilities.httpErrorHandler(reason, $scope);
+        if (reason.status !== 404) {
+          advisingUtilities.httpErrorHandler(reason, $scope);
+        }
       }, function(update) {
         advisingUtilities.updateProgress(update, $scope);
       });
