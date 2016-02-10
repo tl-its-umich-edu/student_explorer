daa02a398a3b115471c0a272ce6d2adef70e00c8
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-02-10 15:52:44 -0500

    Use new list of class site statuses.  Simplify hover JS.  Add link to class site.

--- sespa/app/scripts/directives/bootstrap_status_tooltip.js
+++ sespa/app/scripts/directives/bootstrap_status_tooltip.js
@@ -7,29 +7,27 @@
  * # bootstrapStatusTooltip
  */
 angular.module('sespaApp')
-  .directive('statusTooltip', function(){
+  .directive('statusTooltip', function () {
     return {
       restrict: 'E',
       scope: {
         statusOutput: '=status'
       },
-      link: function(scope, element, attrs){
-        $(element).hover(function(){
-          // on mouseenter
-          if (element.context.childNodes[0].classList[1] === 'green') {
-            element.context.title = 'Encourage';
-          } else if (element.context.childNodes[0].classList[1] === 'yellow') {
-            element.context.title = 'Explore';
-          } else if (element.context.childNodes[0].classList[1] === 'red') {
-            element.context.title = 'Engage';
+      link: function (scope, element, attrs) {
+        $(element).hover(
+          function () {
+            // on mouseenter
+            // TODO: must these element.context attributes be set *every* time?
+            // FIXME: set these three attributes once, outside hover()
+            element.context.title = scope.statusOutput.name;
+            element.context.alt = scope.statusOutput.name;
+            element.context.dataset.placement = 'bottom';
+            $(element).tooltip('show');
+          }, function () {
+            // on mouseleave
+            $(element).tooltip('hide');
           }
-          element.context.alt = element.context.title;
-          element.context.dataset.placement = 'bottom';
-          $(element).tooltip('show');
-        }, function(){
-          // on mouseleave
-          $(element).tooltip('hide');
-        });
+        );
       },
       templateUrl: 'views/status_image.html'
     };
