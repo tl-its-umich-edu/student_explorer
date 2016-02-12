8095ffa2a2fe3f8042e9d6c9533816d1da87234b
Kris Steinhoff <steinhof@umich.edu>
2016-02-12 10:07:55 -0500

    Clean up whitespace and formatting

--- sespa/app/scripts/controllers/sespa_app.js
+++ sespa/app/scripts/controllers/sespa_app.js
@@ -7,16 +7,18 @@
  * # SespaAppCtrl
  * Controller of the sespaApp
  */
- 
+
 angular.module('sespaApp')
   .controller('SespaAppCtrl', function(advisingData, advisingUtilities, $scope, $location) {
     advisingData.config().then(function(config) {
       $scope.config = config;
       $scope.studentSearch = function() {
-        $location.path('/students').search({search: $scope.search});
+        $location.path('/students').search({
+          search: $scope.search
+        });
       };
     });
-    
+
     advisingData.userInfo().then(function(userInfo) {
       $scope.userInfo = userInfo;
     }, function(reason) {
