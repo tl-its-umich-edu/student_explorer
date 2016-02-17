8c5d8f08a5333e75aaa6718bd026e6f1dd1de9b1
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-02-17 14:40:18 -0500

    A possible solution: a JavaScript filter for AngularJS' ng-repeat attribute.  However, if the data coming to the page could be unique before the JS processes it, there could be savings in time and bandwidth.

--- sespa/app/scripts/filters/filters.js
+++ sespa/app/scripts/filters/filters.js
@@ -8,4 +8,13 @@ angular.module('sespaFilters', []).filter('colorToStatus', function () {
       'red': 'Engage',
     }[String(status).toLowerCase()] || null);
   };
+}).filter('unique', function () {
+  return function (items, attr) {
+    var seen = {};
+    return items.filter(function (item) {
+      return (angular.isUndefined(attr) || !item.hasOwnProperty(attr))
+        ? true
+        : seen[item[attr]] = !seen[item[attr]];
+    });
+  };
 });
