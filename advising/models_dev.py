50aec0af68b3e5871b7fa1d1ec9dc95a38e02353
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2016-02-18 14:46:24 -0500

    Revert "Removed unneeded `statuses`"
    This reverts commit 90f854cfb2196c11f809f4e0af8cf15bd5ddbf0f.

--- advising/models_dev.py
+++ advising/models_dev.py
@@ -49,6 +49,8 @@ class Student(models.Model):
     cohorts = models.ManyToManyField('Cohort', through='StudentCohortMentor')
     class_sites = models.ManyToManyField('ClassSite',
                                          through='StudentClassSiteStatus')
+    statuses = models.ManyToManyField('Status',
+                                      through='StudentClassSiteStatus')
 
     def __unicode__(self):
         return self.username
