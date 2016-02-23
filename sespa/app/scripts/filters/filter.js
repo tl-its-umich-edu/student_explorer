220972a69b6b6f423febe28b974eda6f935ca870
weikaizh <weikaizh@umich.edu>
2016-02-23 15:29:39 -0500

    Revise visual performance indicator to assignments table

--- sespa/app/scripts/filters/filter.js
+++ sespa/app/scripts/filters/filter.js
@@ -2,19 +2,25 @@
 
 /**
  * @ngdoc filter
- * @name sespaApp.filter:changeStatus
+ * @name sespaApp.filter:filter
  * @function
  * @description
- * # changeStatus
+ * # filter
  * Filter in the sespaApp.
  */
 angular.module('sespaApp')
-  .filter('changeStatus', function () {
+  .filter('colorToStatus', function () {
     return function (status) {
       return ({
         'green': 'Encourage',
         'yellow': 'Explore',
         'red': 'Engage',
+      }[String(status).toLowerCase()] || null);
+    };
+  })
+  .filter('relativeToMeanDesc', function () {
+    return function (status) {
+      return ({
         'above': 'The student scored at least 5 percentage points higher than the mean.',
         'near': 'The student scored within 5 percentage points of the mean.',
         'below': 'The student scored at least 5 percentage points lower than the mean.',
