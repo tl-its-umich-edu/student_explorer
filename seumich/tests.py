773d4655f74de710965a49a067a3aefad020e205
Kushank Raghav <kushankr@umich.edu>
2016-05-19 12:00:01 -0400

    Add more Model Tests - Covered all models
    This commit has covered all the models. Also used tests from 'advising' app in previous version.

--- seumich/tests.py
+++ seumich/tests.py
@@ -1,15 +1,74 @@
 import os
 from django.test import TestCase
-from seumich.models import UsernameField, Advisor, Date, Mentor, Status, Student, Term, SourceSystem, AdvisorRole, Assignment, ClassSite, Cohort, EventType, ClassSiteTerm, StudentAdvisorRole, StudentCohortMentor, ClassSiteScore, StudentClassSiteAssignment, StudentClassSiteStatus, WeeklyClassSiteScore, WeeklyStudentClassSiteEvent, WeeklyStudentClassSiteStatus, WeeklyStudentClassSiteScore
+from seumich.models import (UsernameField,
+                            Advisor,
+                            Date,
+                            Mentor,
+                            Status,
+                            Student,
+                            Term,
+                            SourceSystem,
+                            AdvisorRole,
+                            Assignment,
+                            ClassSite,
+                            Cohort,
+                            EventType,
+                            ClassSiteTerm,
+                            StudentAdvisorRole,
+                            StudentCohortMentor,
+                            ClassSiteScore,
+                            StudentClassSiteScore,
+                            StudentClassSiteAssignment,
+                            StudentClassSiteStatus,
+                            WeeklyClassSiteScore,
+                            WeeklyStudentClassSiteEvent,
+                            WeeklyStudentClassSiteStatus,
+                            WeeklyStudentClassSiteScore)
+
+
+class UsernameFieldTest(TestCase):
+
+    def test_from_db_value(self):
+        obj = UsernameField()
+        self.assertEqual('grace', obj.from_db_value('Grace', None, None, None))
+        self.assertEqual('james', obj.from_db_value('JAMES', None, None, None))
+
+    def test_get_db_prep_value(self):
+        obj = UsernameField()
+        self.assertEqual('GRACE', obj.get_db_prep_value('Grace', None, None))
+        self.assertEqual('JAMES', obj.get_db_prep_value('james', None, None))
+
+
+class AdvisorTest(TestCase):
+
+    def setUp(self):
+        os.system((
+            'mysql -h 127.0.0.1 -u student_explorer -pstudent_explorer ' +
+            'test_student_explorer < ' +
+            'seumich/fixtures/dev_data_drop_create_and_insert.sql'
+        ))
+
+    def test_advisor_creation(self):
+        """
+        Testing whether the advisor's fetched description
+        matches the expected description
+        """
+        advisor = Advisor.objects.get(id=1)
+        self.assertEqual(str(advisor), 'zander')
+
+
+class DateTest(TestCase):
+
+    def test_date_creation(self):
+        """
+        Testing whether the date's fetched description
+        matches the expected description
+        """
+        date = Date.objects.get(id=2016)
+        self.assertEqual(str(date), '2015-07-09')
 
 
 class MentorTest(TestCase):
-
-    def setUp(self):
-        os.system(
-            'mysql -h 127.0.0.1 -u student_explorer -pstudent_explorer ' +
-            'test_student_explorer < ' +
-            'seumich/fixtures/dev_data_drop_create_and_insert.sql')
 
     def test_cohorts(self):
         """
@@ -29,7 +88,7 @@ class MentorTest(TestCase):
         matches the expected username
         """
         mentor = Mentor.objects.get(id=2)
-        self.assertEqual(mentor.__unicode__(), 'burl')
+        self.assertEqual(str(mentor), 'burl')
 
 
 class StatusTest(TestCase):
@@ -40,7 +99,7 @@ class StatusTest(TestCase):
         matches the expected description
         """
         status = Status.objects.get(id=3)
-        self.assertEqual(status.__unicode__(), 'Red')
+        self.assertEqual(str(status), 'Red')
 
 
 class StudentTest(TestCase):
@@ -59,7 +118,7 @@ class StudentTest(TestCase):
         matches the expected description
         """
         student = Student.objects.get(id=1)
-        self.assertEqual(student.__unicode__(), 'grace')
+        self.assertEqual(str(student), 'grace')
 
 
 class TermTest(TestCase):
@@ -108,7 +167,7 @@ class TermTest(TestCase):
         matches the expected description
         """
         term = Term.objects.get(id=1)
-        self.assertEqual(term.__unicode__(), 'Fall 2015')
+        self.assertEqual(str(term), 'Fall 2015')
 
 
 class SourceSystemTest(TestCase):
@@ -119,7 +178,140 @@ class SourceSystemTest(TestCase):
         matches the expected description
         """
         source_system = SourceSystem.objects.get(code='CNVS')
-        self.assertEqual(source_system.__unicode__(), 'Canvas')
+        self.assertEqual(str(source_system), 'Canvas')
+
+
+class AdvisorRoleTest(TestCase):
+
+    def test_advisor_role_creation(self):
+        """
+        Testing whether the advisor role's fetched description
+        matches the expected description
+        """
+        advisor_role = AdvisorRole.objects.get(id=1)
+        self.assertEqual(str(advisor_role), 'Department Advisor')
+
+
+class AssignmentTest(TestCase):
+
+    def test_assignment_creation(self):
+        """
+        Testing whether the assignment's fetched description
+        matches the expected description
+        """
+        assignment = Assignment.objects.get(id=15)
+        self.assertEqual(str(assignment), 'Quiz extra 2')
+
+
+class ClassSiteTest(TestCase):
+
+    def test_class_site_creation(self):
+        """
+        Testing whether the class site's fetched description
+        matches the expected description
+        """
+        class_site = ClassSite.objects.get(id=1)
+        self.assertEqual(str(class_site), 'Math 101')
+
+
+class CohortTest(TestCase):
+
+    def test_cohort_creation(self):
+        """
+        Testing whether the cohort's fetched description
+        matches the expected description
+        """
+        cohort = Cohort.objects.get(id=3)
+        self.assertEqual(str(cohort), 'Special Probation F15')
+
+
+class EventTypeTest(TestCase):
+
+    def test_event_type_creation(self):
+        """
+        Testing whether the event type's fetched description
+        matches the expected description
+        """
+        event_type = EventType.objects.get(id=1)
+        self.assertEqual(str(event_type), 'session start')
+
+
+class ClassSiteTermTest(TestCase):
+
+    def test_class_site_term_creation(self):
+        """
+        Testing whether the class site term's fetched description
+        matches the expected description
+        """
+        class_site = ClassSite.objects.get(id=2)
+        term = Term.objects.get(id=1)
+        class_site_term = ClassSiteTerm.objects. \
+            filter(class_site=class_site, term=term)
+        self.assertEqual(
+            str(class_site_term[0]),
+            'Math 101 Lab was held in Fall 2015')
+
+
+class StudentAdvisorRoleTest(TestCase):
+    student = Student.objects.get(id=4)
+    advisor = Advisor.objects.get(id=1)
+    role = AdvisorRole.objects.get(id=3)
+
+    def test_student_advisor_role_creation(self):
+        """
+        Testing whether the student advisor role's fetched description
+        matches the expected description
+        """
+        student_advisor_role = StudentAdvisorRole.objects. \
+            filter(student=self.student, advisor=self.advisor, role=self.role)
+        self.assertEqual(str(student_advisor_role[0]),
+                         'zander advises may as Honors Advisor')
+
+
+class StudentCohortMentorTest(TestCase):
+    student = Student.objects.get(id=1)
+    mentor = Mentor.objects.get(id=2)
+    cohort = Cohort.objects.get(id=1)
+
+    def test_student_cohort_mentor_creation(self):
+        """
+        Testing whether the student cohort mentor's fetched description
+        matches the expected description
+        """
+        student_cohort_mentor = StudentCohortMentor.objects. \
+            filter(student=self.student, mentor=self.mentor,
+                   cohort=self.cohort)
+        self.assertEqual(str(student_cohort_mentor[0]),
+                         'grace is in the Special Probation F14 cohort')
+
+
+class ClassSiteScoreTest(TestCase):
+
+    def test_class_site_score_creation(self):
+        """
+        Testing whether the class site score's fetched description
+        matches the expected description
+        """
+        class_site = ClassSite.objects.get(id=2)
+        class_site_score = ClassSiteScore.objects. \
+            filter(class_site=class_site)
+        self.assertEqual(str(class_site_score[0]),
+                         'Math 101 Lab has an average score of 81.9')
+
+
+class StudentClassSiteScoreTest(TestCase):
+    student = Student.objects.get(id=1)
+    class_site = ClassSite.objects.get(id=2)
+
+    def test_student_class_site_score_creation(self):
+        """
+        Testing whether the student class site score's fetched description
+        matches the expected description
+        """
+        student_class_site_score = StudentClassSiteScore.objects. \
+            filter(student=self.student, class_site=self.class_site)
+        self.assertEqual(str(student_class_site_score[0]),
+                         'grace has an average score of 86.3 in Math 101 Lab')
 
 
 class StudentClassSiteAssignmentTest(TestCase):
@@ -137,7 +329,7 @@ class StudentClassSiteAssignmentTest(TestCase):
             filter(student=self.student, class_site=self.class_site,
                    assignment=self.assignment)
         self.assertEqual(
-            student_class_site_assignment[0].__unicode__(),
+            str(student_class_site_assignment[0]),
             'grace has assignment Quiz extra 1 in Math 101 Lab')
 
     def test_studentclasssiteassignment_due_date(self):
@@ -196,3 +388,145 @@ class StudentClassSiteAssignmentTest(TestCase):
         self.assertEqual(None, obj._percentage(10, None))
         self.assertEqual(None, obj._percentage(None, 10))
         self.assertEqual(None, obj._percentage(None, None))
+
+
+class StudentClassSiteStatusTest(TestCase):
+    student = Student.objects.get(id=1)
+    class_site = ClassSite.objects.get(id=2)
+    status = Status.objects.get(id=1)
+
+    def test_studentclasssitestatus_creation(self):
+        """
+        Testing whether the student class site status's fetched
+        description matches the expected description
+        """
+        student_class_site_status = StudentClassSiteStatus. \
+            objects.filter(student=self.student,
+                           class_site=self.class_site,
+                           status=self.status)
+        self.assertEqual(
+            str(student_class_site_status[0]),
+            'grace has status Green in Math 101 Lab')
+
+
+class WeeklyClassSiteScoreTest(TestCase):
+    class_site = ClassSite.objects.get(id=2)
+    week_end_date = Date.objects.get(id=2088)
+
+    def test_weeklyclasssitescore_creation(self):
+        """
+        Testing whether the weekly class site score's fetched description
+        matches the expected description
+        """
+        weekly_class_site_score = WeeklyClassSiteScore.objects. \
+            filter(class_site=self.class_site,
+                   week_end_date=self.week_end_date)
+        self.assertEqual(
+            str(weekly_class_site_score[0]),
+            'Average score is 81 in Math 101 Lab on 2015-09-19')
+
+    def test_WeeklyClassSiteScore(self):
+        c = ClassSite.objects.get(pk=1)
+
+        w = WeeklyClassSiteScore.objects.filter(
+            class_site=c)
+
+        self.assertEqual('Math 101', str(w[0].class_site))
+        self.assertEqual('0', str(w[0].score))
+        self.assertEqual('2015-09-12', str(w[0].week_end_date))
+
+    def test_WeeklyClassSiteScore_date_range(self):
+        c = ClassSite.objects.get(pk=1)
+        t = Term.objects.get(id=1)
+
+        w = WeeklyClassSiteScore.objects.filter(
+            class_site=c,
+            week_end_date__gte=t.begin_date,
+            week_end_date__lte=t.end_date)
+
+        self.assertEqual(8, len(w))
+
+
+class WeeklyStudentClassSiteEventTest(TestCase):
+    student = Student.objects.get(id=1)
+    class_site = ClassSite.objects.get(id=2)
+    week_end_date = Date.objects.get(id=2088)
+
+    def test_weeklystudentclasssiteevent_creation(self):
+        """
+        Testing whether the weekly student class site event's fetched
+        description matches the expected description
+        """
+        weekly_student_class_site_event = WeeklyStudentClassSiteEvent. \
+            objects.filter(student=self.student, class_site=self.class_site,
+                           week_end_date=self.week_end_date)
+        self.assertEqual(
+            str(weekly_student_class_site_event[0]),
+            'grace in Math 101 Lab on 2015-09-19 had 3 events (0.68 %ile)')
+
+    def test_WeeklyStudentClassSiteEvent(self):
+        events = WeeklyStudentClassSiteEvent.objects.filter(
+            student=self.student, class_site=self.class_site)
+
+        self.assertEqual(8, len(events))
+        self.assertEqual('Math 101 Lab', str(events[1].class_site))
+        self.assertEqual('grace', str(events[1].student))
+        self.assertEqual(3, int(events[1].event_count))
+        self.assertEqual('2015-09-19', str(events[1].week_end_date))
+
+
+class WeeklyStudentClassSiteScoreTest(TestCase):
+    student = Student.objects.get(id=1)
+    class_site = ClassSite.objects.get(id=2)
+    week_end_date = Date.objects.get(id=2088)
+
+    def test_weeklystudentclasssitescore_creation(self):
+        """
+        Testing whether the weekly student class site score's fetched
+        description matches the expected description
+        """
+        weekly_student_class_site_score = WeeklyStudentClassSiteScore. \
+            objects.filter(student=self.student, class_site=self.class_site,
+                           week_end_date=self.week_end_date)
+        self.assertEqual(
+            str(weekly_student_class_site_score[0]),
+            'grace has score 62 in Math 101 Lab on 2015-09-19')
+
+    def test_WeeklyStudentClassSiteScore(self):
+        w = WeeklyStudentClassSiteScore.objects.filter(
+            student=self.student, class_site=self.class_site)
+
+        self.assertEqual('Math 101 Lab', str(w[0].class_site))
+        self.assertEqual('grace', str(w[0].student))
+        self.assertEqual('62', str(w[0].score))
+        self.assertEqual('2015-09-12', str(w[0].week_end_date))
+
+
+class WeeklyStudentClassSiteStatusTest(TestCase):
+    student = Student.objects.get(id=1)
+    class_site = ClassSite.objects.get(id=2)
+    week_end_date = Date.objects.get(id=2088)
+    status = Status.objects.get(id=2)
+
+    def test_weeklystudentclasssitestatus_creation(self):
+        """
+        Testing whether the weekly student class site status's fetched
+        description matches the expected description
+        """
+        weekly_student_class_site_status = WeeklyStudentClassSiteStatus. \
+            objects.filter(student=self.student,
+                           class_site=self.class_site,
+                           week_end_date=self.week_end_date,
+                           status=self.status)
+        self.assertEqual(
+            str(weekly_student_class_site_status[0]),
+            'grace has status Yellow in Math 101 Lab on 2015-09-19')
+
+    def test_WeeklyStudentClassSiteStatus(self):
+        w = WeeklyStudentClassSiteStatus.objects.filter(
+            student=self.student, class_site=self.class_site)
+
+        self.assertEqual('Math 101 Lab', str(w[0].class_site))
+        self.assertEqual('grace', str(w[0].student))
+        self.assertEqual('Not Applicable', str(w[0].status))
+        self.assertEqual('2015-09-12', str(w[0].week_end_date))
