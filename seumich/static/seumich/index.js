0d40209ae1d664cc6e882c1de2ac2221e698ce9f
Kushank Raghav <kushankr@umich.edu>
2016-06-02 15:02:44 -0400

    Fix a bug
    Bootstrap disabled does not disable the navigation to url on clicking a disabled link. Fix this using recommended solution - a JS file.

--- seumich/static/seumich/index.js
+++ seumich/static/seumich/index.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('.pagination .disabled a, .pagination .active a').on('click', function(e) {
+        e.preventDefault();
+    });
 });
