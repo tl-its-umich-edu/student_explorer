7b8535591c29ea7f2b4577c9c1720f2204691790
Kris Steinhoff <steinhof@umich.edu>
2016-06-05 23:10:02 -0400

    Update manage.py to re-add conveniences

--- manage.py
+++ manage.py
@@ -4,8 +4,11 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "student_explorer.settings")
+                          "student_explorer.local.development")
 
     from django.core.management import execute_from_command_line
+
+    if len(sys.argv) == 2 and sys.argv[1] == "runserver":
+        sys.argv.append('0.0.0.0:8000')
 
     execute_from_command_line(sys.argv)
