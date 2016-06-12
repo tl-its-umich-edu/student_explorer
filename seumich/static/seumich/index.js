09467db95118a82ab0e110514e415f3ee82d2b26
Kris Steinhoff <steinhof@umich.edu>
2016-06-12 16:13:12 -0400

    Add redirect for hash routes to real URLs

--- seumich/static/seumich/index.js
+++ seumich/static/seumich/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    // Handle old URLs based on hash fragment routing
+    var hash = window.location.hash;
+    if (hash.length > 0) {
+      var url = hash.substring(1);
+      console.log("Hash fragmnet found, redirecting to: " + url);
+      window.location = url;
+
+    }
+
     $('[data-toggle="tooltip"]').tooltip();
 
     $('.pagination .disabled a, .pagination .active a').on('click', function(e) {
