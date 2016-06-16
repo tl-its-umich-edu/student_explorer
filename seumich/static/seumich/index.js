89efb8dfb72f7ec5a824bde832ea6fd5d778de9d
Kushank Raghav <kushankr@umich.edu>
2016-06-16 10:23:20 -0400

    Redo feedback link without Jquery
    Position feedback link using only css

--- seumich/static/seumich/index.js
+++ seumich/static/seumich/index.js
@@ -14,37 +14,4 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-    var changeFeedback = function() {
-        $('.feedback').css('position', 'absolute');
-        $('.feedback').css('margin', '0 auto');
-        // check if a scroll bar is present
-        if ($(document).height() > $(window).height()) {
-            // if scroll, feedback link should be placed absolutely
-            // relative to the body
-            $('body').css('position', 'relative');
-            // if scroll and window width less than 736, reset feedback
-            // position to default
-            if ($(window).width() <= 736) {
-                $('.feedback').css('position', 'static');
-                $('.feedback').css('margin-top', '20px');
-                $('.feedback').css('margin-bottom', '-25px');
-            }
-        } else {
-            // if no scroll, feedback link should be placed absolutely
-            // relative to page  (as height: auto for body)
-            $('body').css('position', 'static');
-            // if no scroll and window width less than 736, set navbar
-            // to fixed as i want it to be at bottom of screen
-            if ($(window).width() <= 736) {
-                $('.navbar-fixed-bottom').css('position', 'fixed');
-            }
-        }
-    };
-
-    changeFeedback();
-
-    $(window).resize(function() {
-        changeFeedback();
-    });
-
 });
