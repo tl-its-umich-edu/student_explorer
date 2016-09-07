bf9c6fffa34ec986136c02f27601ced52cab8fec
Kris Steinhoff <steinhof@umich.edu>
2016-09-07 15:01:58 -0400

    Make logging more robust

--- student_explorer/middleware.py
+++ student_explorer/middleware.py
@@ -8,23 +8,23 @@ class LoggingMiddleware(object):
     def process_response(self, request, response):
         l = []
 
-        l.append(request.META['REMOTE_ADDR'])
+        l.append(request.META.get('REMOTE_ADDR', '-'))
 
-        if request.user.is_authenticated():
+        if hasattr(request, 'user') and request.user.is_authenticated():
             l.append(request.user.username)
         else:
             l.append('-')
 
         l.append(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'))
 
-        l.append('"' + request.META['REQUEST_METHOD'] + ' ' +
+        l.append('"' + request.META.get('REQUEST_METHOD', '-') + ' ' +
                  request.get_full_path() + '"')
 
         l.append(str(response.status_code))
 
         l.append('"' + request.META.get('HTTP_REFERER', '-') + '"')
 
-        l.append('"' + request.META['HTTP_USER_AGENT'] + '"')
+        l.append('"' + request.META.get('HTTP_USER_AGENT', '-') + '"')
 
         logger.info(' '.join(l))
         return response
