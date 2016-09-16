5dffbe5f411468a2383e522ac7d5fc1b00ca8a31
Kushank Raghav <kushankr@umich.edu>
2016-09-16 13:24:18 -0400

    Add and modify charts
    - Merge two bar charts into a line plus bar chart
    - Add functions for weekly and daily unique logins
    - Add function for weekly and daily unique student searches
    - Create a line chart for daily unique student searches

--- usage/views.py
+++ usage/views.py
@@ -22,6 +22,18 @@ class StaffMemberRequiredMixin(object):
                                                               **kwargs)
 
 
+class ExtractUnixTimestamp(Func):
+    function = 'UNIX_TIMESTAMP'
+    template = '%(function)s(%(expressions)s)'
+    output_field = models.IntegerField()
+
+
+class ExtractDate(Func):
+    function = 'DATE'
+    template = '%(function)s(%(expressions)s)'
+    output_field = models.DateField()
+
+
 class ExtractWeek(Func):
     function = 'WEEK'
     template = '%(function)s(%(expressions)s, 3)'
@@ -48,62 +60,117 @@ class ExtractStudent(Func):
 class UsageView(StaffMemberRequiredMixin, TemplateView):
     template_name = 'usage.html'
 
-    def get_weekly_data(self, startdate):
+    def get_weekly_user_data(self, startdate):
         weeklydata = (Event.objects
                       .filter(
                           timestamp__gte=startdate
                       ).annotate(week=ExtractWeek('timestamp'),
                                  year=ExtractYear('timestamp'))
                       .values('week', 'year')
-                      .annotate(usercount=Count('user', distinct=True),
-                                idcount=Count('id'))
+                      .annotate(count=Count('user', distinct=True))
+                      .order_by())
+        return weeklydata
+
+    def get_weekly_hits_data(self, startdate):
+        weeklydata = (Event.objects
+                      .filter(
+                          timestamp__gte=startdate
+                      ).annotate(week=ExtractWeek('timestamp'),
+                                 year=ExtractYear('timestamp'))
+                      .values('week', 'year')
+                      .annotate(count=Count('id'))
                       .order_by())
         return weeklydata
 
     def get_weekly_login_data(self, startdate):
-        weeklylogindata = (Event.objects
+        weeklydata = (Event.objects
                            .filter(
                                name__iexact="UserLoggedIn",
                                timestamp__gte=startdate
                            ).annotate(week=ExtractWeek('timestamp'),
                                       year=ExtractYear('timestamp'))
-                           .values('week', 'year')
-                           .annotate(logincount=Count('user', distinct=True))
-                           .order_by())
-        return weeklylogindata
+                      .values('week', 'year')
+                      .annotate(logincount=Count('user', distinct=True))
+                      .order_by())
+        return weeklydata
 
-    def get_student_search_data(self, startdate):
-        weeklystudentdata = (Event.objects
-                             .filter(
-                                 note__regex=r'^/students/[a-zA-Z]+/',
-                                 timestamp__gte=startdate
-                             )
-                             .annotate(
-                                 week=ExtractWeek('timestamp'),
-                                 year=ExtractYear('timestamp'))
-                             .values('week', 'year')
-                             .annotate(
-                                 studentcount=Count(
-                                     ExtractStudent(
-                                         ExtractSubString('note')),
-                                     distinct=True))
-                             .order_by())
-        return weeklystudentdata
+    def get_daily_login_data(self, startdate):
+        dailydata = (Event.objects
+                          .filter(
+                              name__iexact="UserLoggedIn",
+                              timestamp__gte=startdate
+                          ).annotate(date=ExtractDate('timestamp'))
+                     .values('date')
+                     .annotate(logincount=Count('user', distinct=True))
+                     .order_by())
+        return dailydata
+
+    def get_weekly_student_data(self, startdate):
+        weeklydata = (Event.objects
+                      .filter(
+                          note__regex=r'^/students/[a-zA-Z]+/',
+                          timestamp__gte=startdate
+                      )
+                      .annotate(
+                          week=ExtractWeek('timestamp'),
+                          year=ExtractYear('timestamp'))
+                      .values('week', 'year')
+                      .annotate(
+                          studentcount=Count(
+                              ExtractStudent(
+                                  ExtractSubString('note')),
+                              distinct=True))
+                      .order_by())
+        return weeklydata
+
+    def get_daily_student_data(self, startdate):
+        dailydata = (Event.objects
+                     .filter(
+                         note__regex=r'^/students/[a-zA-Z]+/',
+                         timestamp__gte=startdate
+                     )
+                     .annotate(
+                         date=ExtractUnixTimestamp(
+                             ExtractDate('timestamp')))
+                     .values('date')
+                     .annotate(
+                         studentcount=Count(
+                             ExtractStudent(
+                                 ExtractSubString('note')),
+                             distinct=True))
+                     .order_by())
+        return dailydata
 
     def get_context_data(self, **kwargs):
         context = super(UsageView, self).get_context_data(**kwargs)
         usage_past_weeks = settings.USAGE_PAST_WEEKS
         startdate = timezone.now() - datetime.timedelta(weeks=usage_past_weeks)
-        weeklydata = self.get_weekly_data(startdate)
-        weeklylogindata = self.get_weekly_login_data(startdate)
-        weeklystudentdata = self.get_student_search_data(startdate)
-
-        weekData = []
-        weekData.append(
-            {'key': 'Unique Users Count', 'values': list(
-                weeklydata)}
+        # get next monday and set time to zero
+        startdate = startdate + datetime.timedelta(
+            days=(7 - startdate.weekday())
         )
-        context['weekData'] = weekData
+        startdate = startdate.replace(hour=0, minute=0, second=0)
+        weeklyuserdata = self.get_weekly_user_data(startdate)
+        weeklyhitsdata = self.get_weekly_hits_data(startdate)
+        dailystudentdata = self.get_daily_student_data(startdate)
+
+        weeklyData = []
+        dailyData = []
+        userscount = {'key': 'Unique Users Count',
+                      'values': list(weeklyuserdata),
+                      'bar': 'true',
+                      'color': '#F0D654'}
+        hitscount = {'key': 'Hits Count',
+                     'values': list(weeklyhitsdata),
+                     'color': '#255c91'}
+        dailyData.append(
+            {'key': 'Unique Student Searches', 'values': list(
+                dailystudentdata), 'area': 'true'}
+        )
+        weeklyData.append(userscount)
+        weeklyData.append(hitscount)
+        context['weeklyData'] = weeklyData
+        context['dailyData'] = dailyData
         return context
 
 
