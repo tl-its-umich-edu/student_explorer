cf66d3005cde71bf8d2c626b705304996b10da44
Kushank Raghav <kushankr@umich.edu>
2016-09-16 16:36:30 -0400

    Calculate count for past academic year
    - Calculate count of unique users for past academic year
    - Calculate count of unique students viewed for past academic year
    - Show these counts in the UI

--- usage/views.py
+++ usage/views.py
@@ -13,49 +13,6 @@ import datetime
 import csv
 
 
-class StaffMemberRequiredMixin(object):
-
-    @method_decorator(staff_member_required)
-    def dispatch(self, request, *args, **kwargs):
-        return super(StaffMemberRequiredMixin, self).dispatch(request,
-                                                              *args,
-                                                              **kwargs)
-
-
-class PastAcadDataMixin(object):
-
-    def next_weekday(self, d, weekday):
-        days_ahead = weekday - d.weekday()
-        if days_ahead <= 0:
-            days_ahead += 7
-        return d + datetime.timedelta(days_ahead)
-
-    def get_past_acad_users(self):
-        sept_current = datetime.datetime(timezone.now().year,
-                                         month=9,
-                                         day=1)
-        sept_current = self.next_weekday(sept_current, 0)
-        sept_last = datetime.datetime(timezone.now().year - 1,
-                                      month=9,
-                                      day=1)
-        sept_last = self.next_weekday(sept_last, 0)
-        current_acad_year = timezone.make_aware(
-            sept_current,
-            timezone.get_current_timezone())
-        last_acad_year = timezone.make_aware(
-            sept_last,
-            timezone.get_current_timezone())
-        pastAcadUsers = (Event.objects
-                         .filter(
-                             timestamp__lte=current_acad_year,
-                             timestamp__gte=last_acad_year
-                         )
-                         .values_list('user__username', flat=True)
-                         .distinct()
-                         .order_by())
-        return pastAcadUsers
-
-
 class ExtractUnixTimestamp(Func):
     function = 'UNIX_TIMESTAMP'
     template = '%(function)s(%(expressions)s)'
@@ -80,7 +37,76 @@ class ExtractStudent(Func):
     output_field = models.CharField()
 
 
-class UsageView(StaffMemberRequiredMixin, TemplateView):
+class StaffMemberRequiredMixin(object):
+
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffMemberRequiredMixin, self).dispatch(request,
+                                                              *args,
+                                                              **kwargs)
+
+
+class PastDataMixin(object):
+
+    def __init__(self):
+        self.current = None
+        self.last = None
+
+    def next_weekday(self, d, weekday):
+        days_ahead = weekday - d.weekday()
+        if days_ahead <= 0:
+            days_ahead += 7
+        return d + datetime.timedelta(days_ahead)
+
+    def get_past_acad_year(self):
+
+        sept_current = datetime.datetime(timezone.now().year,
+                                         month=9,
+                                         day=1)
+        sept_current = self.next_weekday(sept_current, 0)
+        sept_last = datetime.datetime(timezone.now().year - 1,
+                                      month=9,
+                                      day=1)
+        sept_last = self.next_weekday(sept_last, 0)
+        current_acad_year = timezone.make_aware(
+            sept_current,
+            timezone.get_current_timezone())
+        last_acad_year = timezone.make_aware(
+            sept_last,
+            timezone.get_current_timezone())
+
+        self.current = current_acad_year
+        self.last = last_acad_year
+
+    def get_past_users(self):
+        self.get_past_acad_year()
+        pastUsers = (Event.objects
+                     .filter(
+                         timestamp__lte=self.current,
+                         timestamp__gte=self.last
+                     )
+                     .values_list('user__username', flat=True)
+                     .distinct()
+                     .order_by())
+        return pastUsers
+
+    def get_past_students(self):
+        self.get_past_acad_year()
+        pastStudents = (Event.objects
+                        .filter(
+                            note__regex=r'^/students/[a-zA-Z]+/',
+                            timestamp__lte=self.current,
+                            timestamp__gte=self.last
+                        )
+                        .annotate(student=ExtractStudent(
+                            ExtractSubString('note')))
+                        .values_list('student', flat=True)
+                        .distinct()
+                        .order_by())
+        return pastStudents
+
+
+class UsageView(StaffMemberRequiredMixin, PastDataMixin, TemplateView):
     template_name = 'usage.html'
 
     def get_daily_user_data(self, startdate):
@@ -133,11 +159,14 @@ class UsageView(StaffMemberRequiredMixin, TemplateView):
         dailyData = []
         dailyData.append(dailystudentdata)
         dailyData.append(dailyuserdata)
+
         context['dailyData'] = dailyData
+        context['usersCount'] = self.get_past_users().count()
+        context['studentsCount'] = self.get_past_students().count()
         return context
 
 
-class DownloadCsvView(View, PastAcadDataMixin):
+class DownloadCsvView(View, PastDataMixin):
 
     def render_to_csv(self):
         response = HttpResponse(content_type='text/csv')
@@ -146,7 +175,7 @@ class DownloadCsvView(View, PastAcadDataMixin):
 
         writer = csv.writer(response)
         writer.writerow(['UserName'])
-        for user in self.get_past_acad_users():
+        for user in self.get_past_users():
             writer.writerow([user])
         return response
 
