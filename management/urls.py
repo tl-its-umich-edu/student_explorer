fa1eb86b8aa171a0a0f81e190c6b762572686e1c
Kris Steinhoff <steinhof@umich.edu>
2017-01-30 13:50:49 -0500

    Refactor url names

--- management/urls.py
+++ management/urls.py
@@ -20,6 +20,6 @@ from . import views
 app_name = 'management'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^users/$', views.UserListView.as_view(), name='user_list'),
-    url(r'^cohorts/$', views.CohortListView.as_view(), name='cohort_list'),
+    url(r'^users/$', views.UserListView.as_view(), name='user-list'),
+    url(r'^cohorts/$', views.CohortListView.as_view(), name='cohort-list'),
 ]
