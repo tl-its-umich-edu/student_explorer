cf30ddebece8f3f45e89e18fa2d55bc002a8fd10
Kris Steinhoff <steinhof@umich.edu>
2017-01-30 13:48:41 -0500

    Add models

--- management/models.py
+++ management/models.py
@@ -7,6 +7,7 @@ class Cohort(models.Model):
     code = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     group = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
 
 
 class Student(models.Model):
