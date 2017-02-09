3fcedf0a8cec075372de7af01215936aae59b5ef
Kris Steinhoff <steinhof@umich.edu>
2017-02-09 14:54:16 -0500

    Add management templates

--- management/views.py
+++ management/views.py
@@ -4,6 +4,11 @@ from django.utils.decorators import method_decorator
 from django.http.response import HttpResponseForbidden
 from django.views.generic import TemplateView, ListView
 
+from .models import Student, Cohort
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class StaffRequiredMixin(object):
     @method_decorator(login_required)
@@ -20,8 +25,10 @@ class IndexView(TemplateView, StaffRequiredMixin):
 
 
 class UserListView(ListView, StaffRequiredMixin):
-    template_name = 'management/index.html'
+    template_name = 'management/user_list.html'
+    model = User
 
 
 class CohortListView(ListView, StaffRequiredMixin):
-    template_name = 'management/index.html'
+    template_name = 'management/cohort_list.html'
+    model = Cohort
