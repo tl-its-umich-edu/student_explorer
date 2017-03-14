3731fac066b09341c090c528b4b695df8bb721fd
Kushank Raghav <kushankr@umich.edu>
2017-03-14 11:32:08 -0400

    Use bootstrap in interface

--- management/forms.py
+++ management/forms.py
@@ -8,6 +8,13 @@ import csv
 class CohortForm(forms.ModelForm):
     members = forms.CharField(widget=forms.Textarea)
 
+    def __init__(self, *args, **kwargs):
+        super(CohortForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+
     def clean_members(self):
         data = self.cleaned_data['members']
         sniffer = csv.Sniffer()
