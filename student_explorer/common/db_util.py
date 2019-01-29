82a92f3293bc9a4a4aa54c37a2e5c51ebb048483
Zhen Qian <zqian@umich.edu>
2019-01-28 21:52:58 -0500

    issue_38: report UDW data updated time and schema version (#65)
    * issue_38: report UDW data updated time and schema version
    
    * issue_38 updated according to code reviews

--- student_explorer/common/db_util.py
+++ student_explorer/common/db_util.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 def get_data_date():
 
     try:
-        c = LearningAnalyticsStats.objects.get(dw_data_nm='Student Explorer Daily Tables')
-        logger.info(c)
-        return c.load_dt
+        c = LearningAnalyticsStats.objects.get(dw_data_nm='UDW Daily Tables')
+        return_date_info = "UDW Last Updated on: " + c.load_dt.replace(microsecond=0).isoformat() + "; Schema: " + c.dw_ownr_nm
+        return return_date_info
     except LearningAnalyticsStats.DoesNotExist:
-        return datetime.min
+        return datetime.min.replace(microsecond=0).isoformat()
