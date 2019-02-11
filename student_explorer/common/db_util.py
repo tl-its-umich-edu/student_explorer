cc82630f6c0ef4309bfdcb072ae133fdb3fdfabd
zqian <zqian@umich.edu>
2019-02-11 16:43:48 -0500

    issue_38 update footer text

--- student_explorer/common/db_util.py
+++ student_explorer/common/db_util.py
@@ -4,16 +4,16 @@ import django
 import logging
 from datetime import datetime
 from dateutil.parser import parse
-
 from seumich.models import (LearningAnalyticsStats,)
 
 logger = logging.getLogger(__name__)
 
 def get_data_date():
-
     try:
         c = LearningAnalyticsStats.objects.get(dw_data_nm='UDW Daily Tables')
-        return_date_info = "UDW Last Updated on: " + c.load_dt.replace(microsecond=0).isoformat() + "; Schema: " + c.dw_ownr_nm
-        return return_date_info
+        return {'data_time': c.load_dt,
+                'data_schema': c.dw_ownr_nm}
     except LearningAnalyticsStats.DoesNotExist:
-        return datetime.min.replace(microsecond=0).isoformat()
+        return {'data_time': datetime.min,
+            'data_schema': 'N/A'}
+    
--- student_explorer/context_processors.py
+++ student_explorer/context_processors.py
@@ -1,7 +1,20 @@
 from student_explorer.common import db_util
 import logging
+from datetime import datetime
+import pytz
 
 logger = logging.getLogger(__name__)
 
 def last_updated(request):
-    return {'last_updated': db_util.get_data_date()}
+    est = pytz.timezone('US/Eastern')
+    utc = pytz.utc
+    fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+
+    results = db_util.get_data_date()
+    data_time = results.get("data_time")
+    data_time.replace(tzinfo=utc)
+
+    data_schema = results.get("data_schema")
+
+    return {'last_updated_1': "Data last updated from Canvas: " + data_time.astimezone(est).strftime(fmt),
+            'last_updated_2': "(UDW: " + data_time.replace(microsecond=0).isoformat() + "; Schema: " + data_schema + ")"}
