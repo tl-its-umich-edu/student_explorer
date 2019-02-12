7e2f545d683dc9ad45d9d0c79bb0015f5be3c01f
zqian <zqian@umich.edu>
2019-02-12 10:43:13 -0500

    issue_38 changes based on code reviews

--- student_explorer/context_processors.py
+++ student_explorer/context_processors.py
@@ -1,19 +1,9 @@
 from student_explorer.common import db_util
 import logging
-import pytz
 
 logger = logging.getLogger(__name__)
 
 def last_updated(request):
-    est = pytz.timezone('US/Eastern')
-    utc = pytz.utc
-    fmt = '%Y-%m-%d %H:%M:%S %Z'
-
     results = db_util.get_data_date()
-    data_time = results.get("data_time")
-    data_time.replace(tzinfo=utc)
-
-    data_schema = results.get("data_schema")
-
-    return {'last_updated_1': "Data last updated from Canvas: " + data_time.astimezone(est).strftime(fmt),
-            'last_updated_2': "(UDW: " + data_time.replace(microsecond=0).isoformat() + "; Schema: " + data_schema + ")"}
+    return {'data_time': results.get("data_time"),
+            'data_schema': results.get("data_schema")}
