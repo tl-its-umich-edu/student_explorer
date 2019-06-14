b65565b29cab87d3ec93ae3d5eb04098197acb7f
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-06-14 17:33:45 -0400

    Adding some fixes based on feedback

--- seumich/static/seumich/sort_table.js
+++ seumich/static/seumich/sort_table.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
         cssGoto  : ".pagenum",
         // remove rows from the table to speed up the sort of large tables.
         // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
-        removeRows: false,
+        removeRows: true, 
         // output string - default is '{page}/{totalPages}';
         // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
-        output: "{startRow} - {endRow} / {filteredRows} ({totalRows})"
+        output: "Page / Total Page(s) : {page} / {totalPages}"
       });
 });
