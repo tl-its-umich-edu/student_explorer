f4f131af18df6147a5776f64b3063aabbf841998
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-09-13 16:49:36 -0400

    Add vary_on_cookie to have different views per user

--- seumich/urls.py
+++ seumich/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from seumich import views
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+
 from django.conf import settings
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
@@ -24,36 +26,36 @@ CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 urlpatterns = [
     url(
         r'^$',
-        cache_page(CACHE_TTL)(views.IndexView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.IndexView.as_view())),
         name='index'),
 
     url(
         r'^advisors/$',
-        cache_page(CACHE_TTL)(views.AdvisorsListView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.AdvisorsListView.as_view())),
         name='advisors_list'),
     url(
         r'^cohorts/$',
-        cache_page(CACHE_TTL)(views.CohortsListView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.CohortsListView.as_view())),
         name='cohorts_list'),
     url(
         r'^advisors/(?P<advisor>[\s\w-]+)/$',
-        cache_page(CACHE_TTL)(views.AdvisorView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.AdvisorView.as_view())),
         name='advisor'),
     url(
         r'^cohorts/(?P<code>[\s\w-]+)/$',
-        cache_page(CACHE_TTL)(views.CohortView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.CohortView.as_view())),
         name='cohort'),
     url(
         r'^classes/(?P<class_site_id>\d+)/$',
-        cache_page(CACHE_TTL)(views.ClassSiteView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.ClassSiteView.as_view())),
         name='class_site'),
     url(r'^students/$',
-        cache_page(CACHE_TTL)(views.StudentsListView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.StudentsListView.as_view())),
         name='students_list'),
     url(r'^students/(?P<student>\w+)/$',
-        cache_page(CACHE_TTL)(views.StudentView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.StudentView.as_view())),
         name='student'),
     url(r'^students/(?P<student>\w+)/class_sites/(?P<classcode>[\w-]+)/$',
-        cache_page(CACHE_TTL)(views.StudentClassSiteView.as_view()),
+        vary_on_cookie(cache_page(CACHE_TTL)(views.StudentClassSiteView.as_view())),
         name='student_class'),
 ]
