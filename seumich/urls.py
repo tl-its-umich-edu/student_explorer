e1e62e598a0503d3499a949f8882d8a31280e5ef
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-09-20 16:29:11 -0400

    Removing python caching for cachalot

--- seumich/urls.py
+++ seumich/urls.py
@@ -15,47 +15,40 @@ Including another URLconf
 """
 from django.conf.urls import url
 from seumich import views
-from django.core.cache.backends.base import DEFAULT_TIMEOUT
-from django.views.decorators.cache import cache_page
-from django.views.decorators.vary import vary_on_cookie
-
-from django.conf import settings
-
-CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 urlpatterns = [
     url(
         r'^$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.IndexView.as_view())),
+        views.IndexView.as_view(),
         name='index'),
 
     url(
         r'^advisors/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.AdvisorsListView.as_view())),
+        views.AdvisorsListView.as_view(),
         name='advisors_list'),
     url(
         r'^cohorts/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.CohortsListView.as_view())),
+        views.CohortsListView.as_view(),
         name='cohorts_list'),
     url(
         r'^advisors/(?P<advisor>[\s\w-]+)/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.AdvisorView.as_view())),
+        views.AdvisorView.as_view(),
         name='advisor'),
     url(
         r'^cohorts/(?P<code>[\s\w-]+)/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.CohortView.as_view())),
+        views.CohortView.as_view(),
         name='cohort'),
     url(
         r'^classes/(?P<class_site_id>\d+)/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.ClassSiteView.as_view())),
+        views.ClassSiteView.as_view(),
         name='class_site'),
     url(r'^students/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.StudentsListView.as_view())),
+        views.StudentsListView.as_view(),
         name='students_list'),
     url(r'^students/(?P<student>\w+)/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.StudentView.as_view())),
+        views.StudentView.as_view(),
         name='student'),
     url(r'^students/(?P<student>\w+)/class_sites/(?P<classcode>[\w-]+)/$',
-        vary_on_cookie(cache_page(CACHE_TTL)(views.StudentClassSiteView.as_view())),
+        views.StudentClassSiteView.as_view(),
         name='student_class'),
 ]
