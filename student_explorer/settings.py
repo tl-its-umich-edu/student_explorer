55d4c7b1e9263e2ac6002a0139cc2b0bfaab4c40
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-09-20 15:43:47 -0400

    Adding cachalot to cache database tables

--- student_explorer/settings.py
+++ student_explorer/settings.py
@@ -33,6 +33,7 @@ ADMINS = [('', config('DJANGO_ERROR_EMAIL', default='vagrant@localhost'))]
 # Application definition
 
 INSTALLED_APPS = (
+    'cachalot',
     'django_ptvsd',
     'django.contrib.admin',
     'registration',
@@ -353,7 +354,7 @@ def show_debug_toolbar(request):
 
 if DEBUG:
     from debug_toolbar import settings as dt_settings
-    DEBUG_TOOLBAR_PANELS = dt_settings.PANELS_DEFAULTS
+    DEBUG_TOOLBAR_PANELS = dt_settings.PANELS_DEFAULTS + ['cachalot.panels.CachalotPanel',]
     DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK" : show_debug_toolbar,}
     INSTALLED_APPS += ('debug_toolbar',)
     MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
